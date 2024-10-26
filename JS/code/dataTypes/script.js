02e2125df2e8a131ed0f90468a13197624d2385c
prasad-bigdp <bigdp585@gmail.com>
2024-10-26 17:04:49 +0530

    update 14.08

---
+++ JS/code/dataTypes/script.js
@@ -0,0 +1,31 @@
+// numbers
+var a = 25;
+console.log(typeof a) //number
+//In numbers you can use Math methods such as abs, sqrt, cbrt, ceil, floor, max,min,round, random,pow,
+// tostring method can convert deminal number into any base
+console.log(a.toString(2));
+//strings
+// strings are used to represent alphanumeric values,
+var str = "Javascript"
+console.log(str, typeof str)// Javascript string
+console.log(str[0] + str[4])//JS
+//string methods
+console.log(str.length) //10
+console.log(str.toUpperCase()) //JAVASCRIPT
+console.log(str.toLowerCase()) //javascript
+console.log(str.concat(" is soo easy"))
+console.log(str.indexOf('s'))
+console.log(str.lastIndexOf('a'));
+console.log()
+console.log(str.split('a'));
+console.log(str.endsWith('J'));
+// create an array of items separted values
+console.log(str.split(''))
+
+console.log(["ram ", "is", "good", "Boy"].join(" "))
+
+
+
+
+
+
---
+++ JS/code/first/script.js
@@ -0,0 +1,23 @@
+console.log("bye world")
+var x = -2.5;
+var p = 5, q = 5.16; console.log(typeof (x))
+console.log(Math.pow(p, 2))
+console.log(Math.floor(q), Math.ceil(q), Math.round(q))
+console.log(Math.round(Math.random() * 10000))
+var a = 2,
+	b = 3
+var c = a + b
+console.log(Math.pow(c, 3), c * c * c, c ** 3)
+
+
+// write a js code  to take two numbers and find a+b,a-b,a*b,a/b and print them in console
+
+
+
+
+
+
+
+
+
+
