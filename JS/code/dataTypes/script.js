ad4765b7e2c1b6333ec11be1debb3f8ca981fb72
prasad-bigdp <bigdp585@gmail.com>
2024-10-26 17:27:51 +0530

    update 14.08

--- JS/code/dataTypes/script.js
+++ JS/code/dataTypes/script.js
@@ -21,8 +21,36 @@ console.log(str.split('a'));
 console.log(str.endsWith('J'));
 // create an array of items separted values
 console.log(str.split(''))
-
 console.log(["ram ", "is", "good", "Boy"].join(" "))
+// "js is so easy".split(' ').length
+console.log(str.replace('a', 'i')) //jivascript
+var x = "sonia";
+x.toUpperCase();
+console.log(x)// strings are immutable
+// boolean  -- to store true/false, we use boolean values. true is considered as 1 and false is considered as 0. we use boolean in condition
+var y = true, z = 5 > 7;
+// types will be convert sometimes by default called implicit conversion or duck typing
+console.log('5' + '4');//54
+console.log('5' + 4) //54 --- '5'+4 ==>'5'+'4'
+console.log(5 - '4'); //1
+console.log(5 - "prasad")//NaN
+console.log(5 + true) //6
+
+
+
+var k="prasad"
+console.log(
+	k[0].toUpperCase() +
+		k[1].toLowerCase() +
+		k[2].toUpperCase() +
+		k[3].toLowerCase() +
+		k[4].toUpperCase() +
+		k[5].toLowerCase(),
+)
+
+
+
+
 
 
 
