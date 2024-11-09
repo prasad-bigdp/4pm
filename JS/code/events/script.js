1c2b8d4f3ee75dc953187892f645f3373494f960
prasad-bigdp <bigdp585@gmail.com>
2024-11-09 17:44:08 +0530

    update

---
+++ JS/code/events/script.js
@@ -0,0 +1,28 @@
+// const gp = document.getElementById('gp')
+// const p = document.getElementById("p")
+// const c = document.getElementById("child")
+// const btn = document.getElementById("btn")
+// gp.addEventListener('click', function ()
+// {
+//     alert("grand parent clicked")
+// })
+// p.addEventListener("click", function () {
+// 	alert(" parent clicked")
+// })
+// c.addEventListener("click", function () {
+// 	alert("child clicked")
+// })
+// btn.addEventListener("click", function (e) {
+//     alert("button clicked")
+//     e.stopPropagation()
+// })
+
+document.getElementById('parent').addEventListener('click', function (e)
+{
+    /* e is event-- click
+    e.target -- what is targeting click --- button
+    e.target.textContent -- text inside button*/
+    alert(e.target.textContent+" got clicked")
+})
+
+
